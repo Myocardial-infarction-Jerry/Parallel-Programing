468d7011c05968f30f4613315d369a36480e172f
Myocardial-infarction-Jerry <liusy228@mail2.sysu.edu.cn>
2024-05-21 01:09:32 +0800

    Add Task8 code

---
+++ Task8/src/ShortestPath.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <thread>
+#include <map>
+#include <queue>
+#include <vector>
+
+#include "csv.h"
+
+class DirectedGraph {
+private:
+    class Node;
+    class Edge;
+
+    class Node {
+    public:
+        Node() = default;
+        Node(const std::string &name) : name(name) {}
+        std::string name;
+        std::map<Node *, double> distance;
+        Edge *finalEdge = nullptr;
+    };
+
+    class Edge {
+    public:
+        Edge() = default;
+        Edge(Node *target, double distance, Edge *nextEdge) : target(target), distance(distance), nextEdge{ nextEdge } {}
+
+        Node *target = nullptr;
+        double distance = std::numeric_limits<double>::max();
+        Edge *nextEdge = nullptr;
+    };
+
+    std::map<std::string, Node *> nodes;
+
+    void dijkstra(Node *start) {
+        std::priority_queue<std::pair<int, Node *>, std::vector<std::pair<int, Node *>>, std::greater<std::pair<int, Node *>>> pq;
+        auto &distance = start->distance;
+
+        pq.push({ 0, start });
+        distance[start] = 0;
+
+        while (!pq.empty()) {
+            auto [dist, node] = pq.top();
+            pq.pop();
+
+            if (distance.find(node) != distance.end() && dist > distance[node])
+                continue;
+
+            for (auto edge = node->finalEdge; edge != nullptr; edge = edge->nextEdge) {
+                double newDist = dist + edge->distance;
+                auto target = edge->target;
+                if (distance.find(target) == distance.end() || newDist < distance[target]) {
+                    distance[target] = newDist;
+                    target->finalEdge = edge;
+                    pq.push({ newDist, target });
+                }
+            }
+        }
+    }
+
+public:
+    DirectedGraph() = default;
+    DirectedGraph(const std::string &csv_path) {
+        io::CSVReader<3> in(csv_path);
+        in.read_header(io::ignore_extra_column, "source", "target", "distance");
+        std::string source; std::string target; double distance;
+        while (in.read_row(source, target, distance)) {
+            if (nodes.find(source) == nodes.end())
+                nodes[source] = new Node(source);
+
+            if (nodes.find(target) == nodes.end())
+                nodes[target] = new Node(target);
+
+            nodes[source]->finalEdge = new Edge(nodes[target], distance, nodes[source]->finalEdge);
+        }
+    }
+
+    void solveShortestPath(bool parallel = true) {
+        if (!parallel) {
+            for (auto &node : nodes)
+                dijkstra(node.second);
+
+            return;
+        }
+
+        std::vector<std::thread> threads;
+        for (auto &node : nodes)
+            threads.push_back(std::thread(&DirectedGraph::dijkstra, this, node.second));
+
+        for (auto &thread : threads)
+            thread.join();
+    }
+};
+
+int main(int argc, char const *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <csv_path>" << std::endl;
+        return 1;
+    }
+
+    auto graph = DirectedGraph(argv[1]);
+
+    auto beginTime = std::chrono::high_resolution_clock::now();
+
+    graph.solveShortestPath();
+
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - beginTime).count();
+    std::cout << "Parallel Time elapsed: " << duration << " milliseconds" << std::endl;
+
+    beginTime = std::chrono::high_resolution_clock::now();
+
+    graph.solveShortestPath(false);
+
+    endTime = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - beginTime).count();
+    std::cout << "Sequential Time elapsed: " << duration << " milliseconds" << std::endl;
+
+    return 0;
+}
+
